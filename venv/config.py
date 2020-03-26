d4f99d992149ab526704e3b80e52db97b06cce82
anastasia-si <nastya484@bk.ru>
2020-03-26 18:34:20 -0400

    config updates

--- venv/config.py
+++ venv/config.py
@@ -1,3 +1,7 @@
 BOT_TOKEN = '664115681:AAEMLz09fHzE7jypPayedIPZxSu-Fsti64Y'
 CHAT_ID = 505968936
-FREQUENCY_IN_SEC = 10  # bot will check online data every 10 seconds
+FREQUENCY_IN_SEC = 3600  # bot will check online data every 1 hour
+
+STATE_NAME = 'MASSACHUSETTS' # check statistics only for MA
+PERCENTAGE_CHANGE = 0.2 # report new numbers only if it is a significant change
+URL = 'https://www.worldometers.info/coronavirus/country/us/'
--- venv/monitor.py
+++ venv/monitor.py
@@ -1,13 +1,11 @@
 from lxml import html
-import requests
+from config import STATE_NAME, PERCENTAGE_CHANGE, URL
 from telegram import bot_send
+import requests
 import time
 
 class CovidCaseMonitor:
 
-    URL = 'https://www.worldometers.info/coronavirus/country/us/'
-    STATE_NAME = 'MASSACHUSETTS'
-    PERCENTAGE_CHANGE = 0.1
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36'
     }
@@ -17,7 +15,7 @@ class CovidCaseMonitor:
         self.total_cases = int(self.get_total_cases())
 
     def get_total_cases(self):
-        page = requests.get(self.URL, headers=self.headers)
+        page = requests.get(URL, headers=self.headers)
         html_content = html.fromstring(page.content)
 
         # from bs4 import BeautifulSoup
@@ -27,7 +25,7 @@ class CovidCaseMonitor:
         table = html_content.xpath("//table[@id='usa_table_countries_today']")[0]
         for tr in table.xpath(".//tr"):
             for td in tr.iterchildren():
-                if self.STATE_NAME in td.text_content().upper():
+                if STATE_NAME in td.text_content().upper():
                     total_cases = td.getnext().text_content()
                     total_cases = total_cases.strip().replace(',', '')
                     return int(total_cases)
@@ -47,11 +45,11 @@ class CovidCaseMonitor:
         self.update_total_cases()
         new_total_cases = self.total_cases
 
-        if True or new_total_cases >= prev_total_cases + self.PERCENTAGE_CHANGE * prev_total_cases:
+        if  new_total_cases >= prev_total_cases + PERCENTAGE_CHANGE * prev_total_cases:
             message = "Significant increase in the number of positive tests for COVID-19 in {0}.\n" \
                       "The total number of cases: {1}. \n" \
                       "(Previous value: {2}) \n" \
-                      "Checked at: {3}".format(self.STATE_NAME.capitalize(),
+                      "Checked at: {3}".format(STATE_NAME.capitalize(),
                                                new_total_cases,
                                                prev_total_cases,
                                                time.ctime())

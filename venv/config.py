418ac98d81027f3b6a53785c9084c95195f5146d
anastasia-si <nastya484@bk.ru>
2020-03-26 00:44:05 -0400

    set time period between checks to 10 sec for heroku

--- venv/config.py
+++ venv/config.py
@@ -1,3 +1,3 @@
 BOT_TOKEN = '664115681:AAEMLz09fHzE7jypPayedIPZxSu-Fsti64Y'
 CHAT_ID = 505968936
-FREQUENCY_IN_SEC = 3600  #bot will check online data every hour
+FREQUENCY_IN_SEC = 10  # bot will check online data every 10 seconds
